48a8f07759d19e64d3a923200a888f7b56355357
Radiodrome <mbrown44@missouriwestern.edu>
2018-03-10 14:31:51 -0600

    Implemented text driver, dept and subject reports

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,10 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
@@ -78,7 +78,7 @@ public class Main {
                     if(i==1)
                         insert("Departments","DEPARTMENT_CODE","DEPARTMENT_NAME",
                                 options.get(j).val(),options.get(j).text());
-                    System.out.println(options.get(j));
+                    System.out.println(options.get(j).val() + " | " + options.get(j).text());
                 }
                 System.out.println("-----------------");
             }
@@ -93,8 +93,6 @@ public class Main {
             String course;
             String title;
             String section;
-            int people;
-            int seatsAvailable;
             ArrayList<Course> courses = new ArrayList<>();
 
             Connection conn = this.connect();
@@ -103,6 +101,7 @@ public class Main {
             while(rs.next()){
                 String dCode = rs.getString("DEPARTMENT_CODE");
                 Document doc = Jsoup.connect("https://aps2.missouriwestern.edu/schedule/Default.asp?tck=201910")
+                        .timeout(100000)
                         .data("course_number","")
                         .data("subject","ALL")
                         .data("department",dCode)
@@ -115,7 +114,7 @@ public class Main {
                     course = tds.get(1).text();
                     section = tds.get(2).text();
                     title = tds.get(4).text();
-                    Course courseObject = new Course(crn,course,section,title);
+                    Course courseObject = new Course(crn,course,title,section);
                     courses.add(courseObject);
                 }
                 Elements detailrows = doc.select("table.results tr.detail_row");
@@ -123,6 +122,10 @@ public class Main {
                     Elements seats = detailrows.get(i).select("span.course_seats");
                     parseSeatText(courses.get(i),seats.get(0));
                 }
+            }
+            System.out.println("Report of Courses");
+            for (Course c: courses) {
+                System.out.println(c.toString());
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -140,12 +143,118 @@ public class Main {
         course.setPeople(maxEnroll-seatsAvail);
     }
 
-    public static void main(String[] args) {
+    public void report(){
+        try {
+            Connection conn = this.connect();
+            String statement = "SELECT * FROM Departments";
+            ResultSet rs = conn.createStatement().executeQuery(statement);
+            System.out.println("::Departments::");
+            while(rs.next()){
+                String dCode = rs.getString("DEPARTMENT_CODE");
+                String dName = rs.getString("DEPARTMENT_NAME");
+                System.out.printf("Dept. Code: %-5s | Dept. Name: %-15s\n", dCode, dName);
+            }
+            statement = "SELECT * FROM Subjects";
+            rs = conn.createStatement().executeQuery(statement);
+            System.out.println("::Subjects::");
+            while(rs.next()){
+                String sCode = rs.getString("SUBJECT_CODE");
+                String sName = rs.getString("SUBJECT_NAME");
+                System.out.printf("Sub. Code: %-5s | Sub. Name: %-15s\n", sCode, sName);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
         Main app = new Main();
-        app.dropTables();
-        app.createTables();
-        app.loadDB();
-        app.postCourse();
+        boolean isQuit = false;
+        boolean isCreated = false;
+        Connection conn = app.connect();
+        try {
+            ResultSet rs = conn.getMetaData().getTables(null,null,"%",null);
+            if(rs.next()){
+                isCreated=true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        while(!isQuit)
+        {
+            System.out.println("Please input command. Enter help for valid commands.");
+
+            Scanner input = new Scanner(System.in);
+
+            String command = input.next();
+
+            switch (command.toLowerCase())
+            {
+                case "help":
+                    System.out.println("This is the help menu");
+                    System.out.println("help - Displays this menu.");
+                    System.out.println("quit - Exits this program.");
+                    System.out.println("create - Creates the Department and Subjects tables.");
+                    System.out.println("load - Scrapes and loads the Department and Subjects tables.");
+                    System.out.println("courses - Scrapes and loads the courses to memory.");
+                    System.out.println("drop - Drops the Department and Subjects tables.");
+                    System.out.println("report - Prints the department report for courses");
+                    break;
+
+                case "quit":
+                    System.out.println("Quitting...");
+                    isQuit = true;
+                    break;
+
+                case "create":
+                    if(!isCreated) {
+                        System.out.println("Creating the Department and Subjects tables...");
+                        app.createTables();
+                        isCreated = true;
+                    } else{
+                        System.out.println("Database has already been created...");
+                    }
+                    break;
+
+                case "load":
+                    if(isCreated) {
+                        System.out.println("Loading the DB...");
+                        app.loadDB();
+                    } else{
+                        System.out.println("Database must be created first...");
+                    }
+                    break;
+
+                case "courses":
+                    if(isCreated){
+                        System.out.println("Scraping courses by department...");
+                        app.postCourse();
+                    } else{
+                        System.out.println("Database must be created first...");
+                    }
+                    break;
+
+                case "drop":
+                    if(isCreated) {
+                        System.out.println("Dropping the Departments and Subjects tables...");
+                        app.dropTables();
+                        isCreated = false;
+                    } else{
+                        System.out.println("Database has already been dropped...");
+                    }
+                    break;
+
+                case "report":
+                    System.out.println("Outputting the report...");
+                    app.report();
+                    break;
+
+                default:
+                    System.out.println("Invalid command. please type help for list of commands.");
+                    break;
+            }
+        }
     }
 
     class Course{
@@ -209,6 +318,12 @@ public class Main {
 
         public void setSeatsAvailable(int seatsAvailable) {
             this.seatsAvailable = seatsAvailable;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("CRN: %s|Code: %s|Name: %s|Section: %s|Enrolls: %d|SeatsAvail: %d",
+                    crn,course,title,section,people,seatsAvailable);
         }
     }
 }
